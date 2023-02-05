df55feb68717a4313efab18d3d137608b493ae66
Eric <ericmkelly@gmail.com>
2023-02-05 13:20:18 -0500

    Add files via upload

---
+++ install-sysmon.bat
@@ -0,0 +1,5 @@
+cd c:\Tools
+
+c:\Tools\sysmon.exe -i C:\cpt171\ionstorm-config.xml -accepteula
+sc config sysmon start= auto
+
--- uninstall-wazuh.bat
+++ uninstall-wazuh.bat
@@ -1,4 +1,3 @@
-cd c:\sysmon
-
-
+net stop "OssecSvc"
+cd c:\Tools
 msiexec.exe /x wazuh-agent-3.13.1-1.msi /qn
